a1c0cf6d80494392f2be36794407d88820a0aa5f
yushanshan123 <38271269+yushanshan123@users.noreply.github.com>
2018-04-18 03:31:45 +0000

    Update 31.js

--- 31.js
+++ 31.js
@@ -4,5 +4,5 @@ var source=tu.getAttribute("href");
 var text=tu.getAttribute("title");
 var des=document.getElementById("description");
 document.getElementById("bottom").setAttribute("src",source);
-des.fristchild.nodeValue=text;
+des.firstChild.nodeValue=text;
 }
