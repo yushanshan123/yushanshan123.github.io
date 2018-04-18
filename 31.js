4d892a056bf398774ec8f0e4c0a30e722284fde8
yushanshan123 <38271269+yushanshan123@users.noreply.github.com>
2018-04-18 03:16:06 +0000

    Update 31.js

--- 31.js
+++ 31.js
@@ -1,5 +1,8 @@
 function showpic(tu)
 {
 var source=tu.getAttribute("href");
+var text=tu.getAttribute("title");
+var description=document.getElementById("description");
 document.getElementById("bottom").setAttribute("src",source);
+description.fristchild.nodeValue=text;
 }
