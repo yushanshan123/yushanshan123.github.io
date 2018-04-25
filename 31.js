31e460b9ef1904687e349715ccc5f8eff693ca09
yushanshan123 <38271269+yushanshan123@users.noreply.github.com>
2018-04-25 11:28:19 +0800

    Delete 31.js

--- 31.js
+++
@@ -1,8 +0,0 @@
-function showpic(tu)
-{
-var source=tu.getAttribute("href");
-var text=tu.getAttribute("title");
-var des=document.getElementById("description");
-document.getElementById("bottom").setAttribute("src",source);
-des.firstChild.nodeValue=text;
-}
