db977cad88549fe9f64627a62970214159fd84c6
yushanshan123 <38271269+yushanshan123@users.noreply.github.com>
2018-05-09 11:14:02 +0800

    Add files via upload

---
+++ js/5.js
@@ -0,0 +1,25 @@
+function showpic(tu)
+{
+var source=tu.getAttribute("href");
+document.getElementById("bottom").setAttribute("src",source);
+var text=tu.getAttribute("title");
+document.getElementById("description").firstChild.nodeValue=text;
+}
+function prepareGallery()
+{
+ if(!document.getElementsByTagName) return false;
+ if(!document.getElementById) return false;
+ if(!document.getElementsById("imagegallery")) return false;
+ var gallery=document.getElementById("imagegallery");
+ var links=gallery.getElementsByTagName("a");
+ for(var i=0;i<links.length;i++)
+ {
+   links[i].onclick=function()
+                {
+                 showpic(this);
+                 return false;
+                }
+ }
+
+
+}
