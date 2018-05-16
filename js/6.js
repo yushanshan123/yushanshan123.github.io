4efdb38083672f5056c413c99edc015c4aa7439f
yushanshan123 <38271269+yushanshan123@users.noreply.github.com>
2018-05-16 11:39:21 +0800

    Update 6.js

--- js/6.js
+++ js/6.js
@@ -1,1 +1,38 @@
-555
+function showpic(tu)
+{
+var source=tu.getAttribute("href");
+document.getElementById("bottom").setAttribute("src",source);
+var text=tu.getAttribute("title");
+document.getElementById("description").firstChild.nodeValue=text;
+}
+function prepareGallery()
+{
+ if(!document.getElementsByTagName) return false;
+ if(!document.getElementById) return false;
+ if(!document.getElementById("imagegallery")) return false;
+ var gallery=document.getElementById("imagegallery");
+ var links=gallery.getElementsByTagName("a");
+ for(var i=0;i<links.length;i++)
+ {
+   links[i].onclick=function()
+                {
+                 showpic(this);
+                 return false;
+                }
+ }
+}
+
+function addLoadEvent(func)
+{
+  var oldnload=window.onload;
+  if(typeof wiondow.onload!='function')
+  {
+    window.onload=func;
+  }
+  else
+  {
+  window.onload=fonction(){
+   
+  }
+  }
+}
