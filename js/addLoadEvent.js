35773490785bc52e7ed8ec3b6acbfd00ad9d5829
yushanshan123 <38271269+yushanshan123@users.noreply.github.com>
2018-05-30 10:41:13 +0800

    Update addLoadEvent.js

--- js/addLoadEvent.js
+++ js/addLoadEvent.js
@@ -1,1 +1,12 @@
-<112>
+function addLoadEvent(func)
+{
+ var oldonload = window.onload;
+ if( typeof window.onload != 'function'){
+    window.onload = func;}
+ else{
+ window.onload = function(){
+                             oldonload();
+                             func();
+                            }
+     }
+}
